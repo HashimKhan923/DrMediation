3996aa79f397c0ef369d43691e00d3a8b12b3f06
HashimKhan923 <khanhash1994@gmail.com>
2023-06-26 14:18:43 +0500

    change ing

--- app/Http/Controllers/User/SurveyController.php
+++ app/Http/Controllers/User/SurveyController.php
@@ -33,6 +33,29 @@ class SurveyController extends Controller
         return response($response, 200);
     }
 
+    public function app_create(Request $request)
+    {
+        Survey::where('user_id',$request->user_id)->delete();
+
+        $data = json_decode($request, true);
+
+        $user_id = $data['user_id'];
+        $category_id = $data['category_id'];
+        $subcategory_ids = $data['subcategory_id'];
+
+        foreach($subcategory_ids as $subcategory_id)
+        {
+            $new = new Survey();
+            $new->user_id = $user_id;
+            $new->category_id = $category_id;
+            $new->subcategory_id = $subcategory_id;
+            $new->save();
+        }
+
+        $response = ['status'=>true,"message" => "New Survey Added Successfully!"];
+        return response($response, 200);
+    }
+
 
 
 }
