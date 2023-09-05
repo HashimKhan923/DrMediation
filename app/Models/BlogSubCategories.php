faeb43410f481ff7b902508c21a8dfba8b437b1b
unknown <HashimKhan923>
2023-09-05 19:35:51 +0500

    wewe

--- app/Models/BlogSubCategories.php
+++ app/Models/BlogSubCategories.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class BlogSubCategories extends Model
 {
     use HasFactory;
+
+    public function sub_category()
+    {
+        return $this->belongsTo(SubCategory::class,'subcategory_id','id');
+    } 
 }
