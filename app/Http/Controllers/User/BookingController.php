13980eb897382184137a1099bb866a405290c25a
unknown <HashimKhan923>
2023-09-26 15:02:01 +0500

    dwdwe

--- app/Http/Controllers/User/BookingController.php
+++ app/Http/Controllers/User/BookingController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Booking;
+use App\Models\User;
+use App\Models\Slot;
+use Mail;
 
 class BookingController extends Controller
 {
@@ -18,7 +21,31 @@ class BookingController extends Controller
         $new->date = $request->date;
         $new->save();
 
-        $response = ['status'=>true,"message" => "Booked Successfully!"];
+
+        $user = User::where('id',$new->user_id)->first();
+        $advisor = User::where('id',$new->advisor_id)->first();
+        $slot = Slot::where('id',$new->slot_id)->first();
+
+
+        Mail::send(
+            'email.confirm_booking',
+            [
+                'name'=>$user->name,
+                'advisor'=>$advisor,
+                'slot'=>$slot,
+                'service'=>$new->service,
+                'date'=>$new->date,
+            ], 
+        
+        function ($message) use ($user) {
+            $message->from(env('MAIL_USERNAME'));
+            $message->to($user->email);
+            $message->subject('Confirm Booking');
+        });
+
+
+
+        $response = ['status'=>true,"message" => "Booked Successfully! The confirmation email has been sent to your email."];
         return response($response, 200);
 
     }
---
+++ resources/views/email/confirm_booking.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Slot Booking Confirmation</title>
+</head>
+<body>
+    <table width="100%" cellpadding="0" cellspacing="0">
+        <tr>
+            <td align="center" bgcolor="#f4f4f4">
+                <table width="600" cellpadding="0" cellspacing="0">
+                    <!-- Header Section -->
+                    <tr>
+                        <td align="center" bgcolor="#DBEEC5">
+                            <img src="{{asset('drlogo.png')}}" alt="Dr. Meditation Logo" width="180" height="180" style="display: block; margin: 20px auto;">
+                            <h1 style="color: #fff; font-size: 24px;">Slot Booking Confirmation</h1>
+                        </td>
+                    </tr>
+                    <!-- Content Section -->
+                    <tr>
+                        <td bgcolor="#ffffff" style="padding: 40px; font-size: 16px; line-height: 1.6;">
+                            <p>Hello {{$name}},</p>
+                            <p>Your slot booking with Dr. Meditation has been confirmed. We look forward to meeting you!</p>
+                            <p>Here are the details of your booking:</p>
+                            <h4>Timing Details</h4>
+                            <ul>
+                                <li>Date : {{$date}}</li>
+                                <li>Time: {{$slot->time}}</li>
+                            </ul>
+
+                            <h4>Advisor Details</h4>
+                            <ul>
+                                <li>Name : {{$advisor->name}}</li>
+                                <li>Email: {{$advisor->email}}</li>
+                            </ul>
+
+                            <h4>Service Details</h4>
+                            <ul>
+                                <li>Name : {{$service}}</li>
+                            </ul>
+                            <p>At Dr. Meditation, we are dedicated to helping people lead healthier and happier lives through mindfulness and meditation. Your participation is important to us, and we are excited to have you join us.</p>
+                            <p>If you have any questions or need further information, please don't hesitate to contact us.</p>
+                            <p>Once again, thank you for choosing Dr. Meditation. We look forward to seeing you at the scheduled date and time.</p>
+                            <p>Best regards,</p>
+                            <p>The Dr. Meditation Team</p>
+                        </td>
+                    </tr>
+                    <!-- Footer Section -->
+                    <tr>
+                        <td bgcolor="#007bff" style="padding: 20px; text-align: center; font-size: 14px; color: #fff;">
+                            &copy; 2023 Dr. Meditation. All rights reserved.
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
