42a1eae364e1d916c9d95c2e36422bb9faf74ccb
HashimKhan923 <khanhash1994@gmail.com>
2024-03-18 17:08:17 +0500

    wwqwwssxxsqqdwdwsddzzdds

--- app/Http/Controllers/Admin/UserManagmentController.php
+++ app/Http/Controllers/Admin/UserManagmentController.php
@@ -44,4 +44,14 @@ class UserManagmentController extends Controller
 
     }
 
+    public function is_phone($id)
+    {
+        $update=User::where('phone_number',$id)->first();
+        $update->is_phone = 1;
+        $update->save();
+
+        $response = ['status'=>true,"message" => "Phone number verifed Successfully!"];
+        return response($response, 200);
+    }
+
 }
